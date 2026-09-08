fc5e3c2d8680c0fc32f914f6567be36b3f5e0605
Rithick Ashokkumar <rithickcareer@18@gmail.com>
2026-09-08 20:07:09 +0000

    Build TaskFlow application with Docker

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,19 @@
 from flask import Flask, jsonify, request
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 
 def get_db_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="taskflow",
-        user="taskflow_user",
-        password="Taskflow@123"
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
 
 
